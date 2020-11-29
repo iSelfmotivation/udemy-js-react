58c54ac374962f478c9dcd154b482481d1b1cdd7
iSelfmotivation <mira55mohax133@gmail.com>
2020-11-30 01:26:04 +0400

    refactor: apply functions to object as methods

--- script.js
+++ script.js
@@ -1,74 +1,66 @@
 'use strict';
 
-// if (!numberOfFilms) {
-   
-// }
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
-};
+    start: function() {
+        this.count = +prompt("Сколько фильмов вы уже просмотрели?", "");
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const lastMovie = prompt("Один из последних просмотренных фильмов?", "");
-        const ratingOfLastMovie = +prompt("На сколько оцените его?", "0");
-        if (lastMovie != null &&
-            lastMovie != '' &&
-            ratingOfLastMovie != null &&
-            ratingOfLastMovie != '' &&
-            lastMovie.length < 50) {
-            personalMovieDB.movies[lastMovie] = ratingOfLastMovie;
-        } else {
-            i--;
+        while (this.count == '' || this.count == null || isNaN(this.count)) {
+            this.count = +prompt("Сколько фильмов вы уже просмотрели?", "");
         }
-    }
-}
-
-rememberMyFilms();
-
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 || personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i <=3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.genres[i - 1] = genre;
-    }
-}
-
-writeYourGenres();
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const lastMovie = prompt("Один из последних просмотренных фильмов?", "");
+            const ratingOfLastMovie = +prompt("На сколько оцените его?", "0");
+            if (lastMovie != null &&
+                lastMovie != '' &&
+                ratingOfLastMovie != null &&
+                ratingOfLastMovie != '' &&
+                lastMovie.length < 50) {
+                personalMovieDB.movies[lastMovie] = ratingOfLastMovie;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 || personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <=3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            while (genre == '' || genre == null) {
+                genre = +prompt(`Ваш любимый жанр под номером ${i}`);
+            }
+            personalMovieDB.genres[i - 1] = genre;
+        }
+        this.genres.forEach((genre, i) => {
+            console.log(`Любимый жанр #${i+1} - это ${genre}`);
+        });
+    },
+    toggleVisibleMyDB: function() {
+        if (this.privat) {
+            this.privat = false;
+        } else {
+            this.privat = true;
+        }
+    },
+};
